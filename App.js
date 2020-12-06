5453174d67f32bfd1b2074005bafe38d1791b6ba
jwy <jwy@jwydeMac.local>
2020-12-05 23:00:25 -0800

    update

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: jwy
+ * @Date: 2020-12-05 19:18:13
+ * @LastEditors: jwy
+ * @LastEditTime: 2020-12-05 22:09:38
+ */
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  Platform,
+  SafeAreaView,
+  NativeModules,
+  StatusBar,
+} from 'react-native';
+const {StatusBarManager} = NativeModules;
+// 获取状态栏高度
+let statusBarHeight;
+if (Platform.OS === 'ios') {
+  StatusBarManager.getHeight((height) => {
+    statusBarHeight = height;
+  });
+} else {
+  statusBarHeight = StatusBar.currentHeight;
+}
 
 const App = () => {
   return (
-    <View>
-      <Text>welcome</Text>
-    </View>
+    <SafeAreaView style={{marginTop: statusBarHeight.height}}>
+      <View>
+        <Text>welcome11111</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: jwy
+ * @Date: 2020-12-05 19:18:13
+ * @LastEditors: jwy
+ * @LastEditTime: 2020-12-05 21:51:10
+ */
 /**
  * Metro configuration for React Native
  * https://github.com/facebook/react-native
@@ -6,6 +14,10 @@
  */
 
 module.exports = {
+  resolver: {
+    /* resolver options */
+    sourceExts: ['jsx', 'js', 'ts', 'tsx'],
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
